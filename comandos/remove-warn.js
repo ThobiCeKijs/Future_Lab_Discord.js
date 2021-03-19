ad3e8c47e61ebbb4897c2b7d8b96b9c54ac5777a
Kira-Devv <80856939+Kira-Devv@users.noreply.github.com>
2021-03-19 02:19:38 -0300

    Create remove-warn.js

---
+++ comandos/remove-warn.js
@@ -0,0 +1,27 @@
+const db = require('quick.db');
+const warnings = require('./warnings');
+
+module.exports = {
+    name: "removewarns",
+    description: "Excluir avisos de um membro",
+
+
+    async run (client, message, args){
+        if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Você não pode usar isso.');
+
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+
+        if(!user) return message.channel.send('Especifique um usuário, por meio de menção ou ID');
+
+        if(user.bot) return message.channel.send('Você não pode avisar os bots');
+
+        if(user.id === message.author.id) return message.channel.send('Você não pode limpar seus próprios avisos');
+
+        if(warnings === null) return message.channel.send(`**${user.username} não tem avisos**`);
+
+
+        db.delete(`warnings_${message.guild.id}_${user.id}`);
+
+        message.channel.send('Sucesso!')
+    }
+}
