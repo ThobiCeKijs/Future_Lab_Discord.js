807f4561fabc8d2af83c25c0b90a6ba985917452
Kira-Devv <80856939+Kira-Devv@users.noreply.github.com>
2021-03-19 22:18:49 -0300

    Create dado.js

---
+++ comandos/dado.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+    var numero = Math.floor(Math.random() * 6) + 1;
+
+    message.channel.send(`Você jogou o dado e... Caiu em: \`${numero}\``)
+}
